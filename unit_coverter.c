493b273980699c93eecc9577d693171a0a63207b
srishtibadam2008-commits <srishtibadam2008@gmail.com>
2026-09-14 01:03:40 +0530

    Update unit_coverter.c

--- unit_coverter.c
+++ unit_coverter.c
@@ -1,1 +1,15 @@
+ #include <stdio.h>
+int main() {
+double fahrenheit, celsius;
 
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%lf", &fahrenheit);
+
+    celsius = (fahrenheit - 32) * 5 / 9;
+
+    printf("\n--- Conversion Result ---\n");
+    printf("Fahrenheit: %.2lf F\n", fahrenheit);
+    printf("Celsius:    %.2lf C\n", celsius);
+
+    return 0;
+}
